5f86b6a028b7ebcc00618151169093d89f28787e
LAPTOP-D31L41D6\elyovs <elyovsun@gmail.com>
2024-06-01 01:51:02 +0700

    remove unused import

--- lib/chart.dart
+++ lib/chart.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class ChartScreen extends StatefulWidget {
   const ChartScreen({ Key? key }) : super(key: key);
