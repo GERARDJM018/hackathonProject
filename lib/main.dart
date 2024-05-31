9df8ef3f59dd1bbc73873e5489af7344aa0a1eb8
GERARDJM018 <e0985922@u.nus.edu>
2024-06-01 03:15:39 +0800

    Merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-=======
 import 'package:firebase_auth/firebase_auth.dart';
->>>>>>> branch-firebase
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:hackathon2024/auth/auth.dart';
 import 'package:hackathon2024/firebase_option.dart';
 import 'package:hackathon2024/widget_tree.dart';
 import 'package:hackathon2024/widgets/report_card.dart';
-=======
-import 'package:hackathon2024/chart.dart';
->>>>>>> branch-bar-chart
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,14 +24,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-<<<<<<< HEAD
       home: const WidgetTree(),
     );  
-=======
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: ChartScreen(),
-    );
->>>>>>> branch-bar-chart
   }
 }
 
